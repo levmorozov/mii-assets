d2c314ee999c71769b65997fccdce605bee85d16
lev <mail@levmorozov.ru>
2022-11-27 00:54:43 +0300

    esbuild

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 npm update
 cp node_modules/@swc/core-linux-x64-gnu/swc ./bin/
 cp node_modules/lightningcss-cli/lightningcss ./bin/
+cp node_modules/esbuild-linux-64/bin/esbuild ./bin/
 chmod +x bin/*
